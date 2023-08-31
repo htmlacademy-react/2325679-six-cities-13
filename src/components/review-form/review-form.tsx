da92a8e3d3a3ffae78721b583f7f7f3bf4d15a9e
Анна Андреева <andreeva.anna2909@gmail.com>
2023-08-31 19:17:29 +0400

    Внесена правка

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -106,7 +106,7 @@ function ReviewForm({ id }: ReviewFormProps): JSX.Element {
             To submit review please make sure to set{' '}
             <span className="reviews__star">rating</span> and describe
             your stay with at least{' '}
-            <b className="reviews__text-amount">50 characters</b>.
+            <b className="reviews__text-amount">{CommentLengthLimit.Minimum} characters</b>.
           </p>
           <button
             className="reviews__submit form__submit button"
